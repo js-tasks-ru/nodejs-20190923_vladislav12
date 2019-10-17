fd6fa7cf92377c9a877c72eae72e90ae074a1ab9
DvoVlad <dvovlad@mail.ru>
2019-10-17 09:31:50 +0300

    add solution for 1-module 1-task

--- 4-module/2-task/server.js
+++ 4-module/2-task/server.js
@@ -1,6 +1,8 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+const LimitSizeStream = require('./LimitSizeStream');
 
 const server = new http.Server();
 
@@ -11,7 +13,24 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'POST':
-
+	if (pathname.includes('/')) {
+    		res.statusCode = 400;
+    		res.end('internal path is not allowed');
+  	}
+	fs.writeFile(filepath, req.body, (err) => {
+	// throws an error, you could also catch it here
+    	if (err){ 
+			res.statusCode = 500;
+			throw err;
+		}
+		
+    	// success case, the file was saved
+		res.statusCode = 200;
+		res.end('Saved!');
+	}).pipe(LimitSizeStream)
+	.on('error', err=>{
+		if(err) throw err;
+	});
       break;
 
     default:
